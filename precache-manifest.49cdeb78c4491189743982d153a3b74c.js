213a1a976e2fe85c38ecbf9d9e7e93bf6e317e44
AnnaSv <anna.sviatelyk@gmail.com>
2020-07-08 17:11:37 +0400

    Updates

---
+++ precache-manifest.49cdeb78c4491189743982d153a3b74c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd1b3a673add23ea6182d11220dc4fb",
+    "url": "/my-to-do-list-react/index.html"
+  },
+  {
+    "revision": "5dc3a1e7b667f7872328",
+    "url": "/my-to-do-list-react/static/css/main.56ee7e3a.chunk.css"
+  },
+  {
+    "revision": "0c80843bd41ae70f9dee",
+    "url": "/my-to-do-list-react/static/js/2.539ab8f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-to-do-list-react/static/js/2.539ab8f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc3a1e7b667f7872328",
+    "url": "/my-to-do-list-react/static/js/main.3f1d088e.chunk.js"
+  },
+  {
+    "revision": "39103dc14ff159f1ade8",
+    "url": "/my-to-do-list-react/static/js/runtime-main.545ebdef.js"
+  },
+  {
+    "revision": "ee73dd7864bd6deb640fd193898b5e23",
+    "url": "/my-to-do-list-react/static/media/cancel.ee73dd78.svg"
+  },
+  {
+    "revision": "f97a6fdbfb11d0b2ef67afd600098751",
+    "url": "/my-to-do-list-react/static/media/done.f97a6fdb.svg"
+  },
+  {
+    "revision": "5766fa4e6e3903826734fbf7e557f493",
+    "url": "/my-to-do-list-react/static/media/edit.5766fa4e.svg"
+  },
+  {
+    "revision": "bfcc655088e236247a3bcfb5a6660325",
+    "url": "/my-to-do-list-react/static/media/illustration.bfcc6550.svg"
+  },
+  {
+    "revision": "5a68cbf4ec8f3c54f3b4241fe9080a1f",
+    "url": "/my-to-do-list-react/static/media/plus.5a68cbf4.svg"
+  },
+  {
+    "revision": "55c4bcca1353048c6f4ef473f1148c98",
+    "url": "/my-to-do-list-react/static/media/trash.55c4bcca.svg"
+  }
+]);
