1bc71ed4b24f51b2570d5d52eab562cf1aad46b2
AnnaSv <anna.sviatelyk@gmail.com>
2020-12-17 13:26:49 +0400

    fixed redux extension registration

--- src/index.js
+++ src/index.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import authReducer from './store/reducers/auth'
-import todoReducer from './store/reducers/toDoList'
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import authReducer from "./store/reducers/auth";
+import todoReducer from "./store/reducers/toDoList";
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const extension = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
+const composeEnhancers =
+  process.env.NODE_ENV === "development" && extension ? extension : compose;
 
 const rootReducer = combineReducers({
   todo: todoReducer,
-  auth: authReducer
-})
+  auth: authReducer,
+});
 
-const store = createStore(rootReducer, composeEnhancers(
-  applyMiddleware(thunk)
-))
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 const app = (
   <Provider store={store}>
-    <HashRouter basename='/'>
+    <HashRouter basename="/">
       <App />
     </HashRouter>
   </Provider>
-)
+);
 
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById("root"));
 
 serviceWorker.unregister();
